953d7eb1c44480040bfda74aeee898797bcd88fc
Dawson Rooney <dawsonr@ku.edu>
2020-03-05 13:02:27 -0600

    start password html and js

---
+++ password.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let firstPassword = document.querySelector("#firstPassword");
+  let confirmPassword = document.querySelector("#confirmPassword");
+  let validate = document.querySelector("#validate");
+  let errorMessage = document.querySelector("#errorMessage");
+})
