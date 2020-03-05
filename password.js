2c339d5c4d77ab5438a9fdca2c666c578addea6a
Dawson Rooney <dawsonr@ku.edu>
2020-03-05 13:23:34 -0600

    complete password.html and js

--- password.js
+++ password.js
@@ -3,4 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
   let confirmPassword = document.querySelector("#confirmPassword");
   let validate = document.querySelector("#validate");
   let errorMessage = document.querySelector("#errorMessage");
+  validate.addEventListener("click", () => {
+    let error = "";
+    if(firstPassword.value.length < 8)
+    {
+      error = error+"password length must be at least 8 characters; "
+    }
+    if(firstPassword.value != confirmPassword.value)
+    {
+      error = error+"Passwords do not match; "
+    }
+    if(error == "")
+    {
+      error = "good password"
+    }
+    errorMessage.innerText = error;
+  })
 })
