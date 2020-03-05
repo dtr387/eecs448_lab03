b224f81fb1a53c35172dc07d3b9ca5a7f2dde709
Dawson Rooney <dawsonr@ku.edu>
2020-03-05 13:43:37 -0600

    start slideshow html and js

---
+++ slideShow.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let theImage = document.querySelector("#theImage");
+  let previous = document.querySelector("#previous");
+  let next = document.querySelector("#next");
+})
